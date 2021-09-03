8482a4a59500cb4b071defe428a4c2fd7eea1202
Debabrata Saha <sahadebabrata570@gmail.com>
2021-09-04 03:30:30 +0600

    verified match and used function to reduce duplcate

--- js/app.js
+++ js/app.js
@@ -21,7 +21,6 @@ document.getElementById('key-pad').addEventListener('click', function (event) {
     const number = event.target.innerText;
     const calcInput = document.getElementById('typed-numbers');
     if (isNaN(number)) {
-        console.log(number)
         if (number == 'C') {
             calcInput.value = ''
         }
@@ -33,4 +32,20 @@ document.getElementById('key-pad').addEventListener('click', function (event) {
 
     }
 })
+
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const successMessage = document.getElementById('notify-success');
+    const failError = document.getElementById('notify-fail');
+
+    if (pin == typedNumbers) {
+        successMessage.style.display = 'block'
+        failError.style.display = 'none'
+    }
+    else {
+        failError.style.display = 'block'
+        successMessage.style.display = 'none'
+    }
+}
 
